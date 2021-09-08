85ffb2524823f1dd6c7efec1ba85428b75b85e31
Electr0Bear <dmitry.kn@protonmail.com>
2021-09-08 17:09:40 +0300

    стилизует шапку под десктоп

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,22 @@
 const navButton = document.querySelector('.page-header__menu-button');
 const navButtonText = document.querySelectorAll('.page-header__menu-button-open, .page-header__menu-button-close');
 const navMenu = document.querySelector('.page-header__navigation');
-const navMenuElement = document.querySelectorAll('.page-header__menu-element');
+const navMenuElement = document.querySelectorAll('.page-header__nav-list, .page-header__auth-item, .page-header__socials-list');
 const navLogo = document.querySelectorAll('.page-header__logo-regular, .page-header__logo-blue');
+
+document.addEventListener('scroll', () => {
+  if (window.pageYOffset > 40 && navMenu.classList.contains('page-header__navigation--closed') && !navMenu.classList.contains('page-header__navigation--scrolled')) {
+    navMenu.classList.toggle('page-header__navigation--scrolled');
+    navLogo[0].setAttribute('hidden', 'hidden');
+    navLogo[1].removeAttribute('hidden', 'hidden');
+    navMenuElement[0].classList.toggle('page-header__nav-list--scrolled');
+  } else if (window.pageYOffset < 40 && navMenu.classList.contains('page-header__navigation--closed') && navMenu.classList.contains('page-header__navigation--scrolled')) {
+    navMenu.classList.remove('page-header__navigation--scrolled');
+    navLogo[1].setAttribute('hidden', 'hidden');
+    navLogo[0].removeAttribute('hidden', 'hidden');
+    navMenuElement[0].classList.remove('page-header__nav-list--scrolled');
+  }
+})
 
 navButton.addEventListener('click', evt => {
   evt.preventDefault();
@@ -10,33 +24,59 @@ navButton.addEventListener('click', evt => {
     console.log('меню открывается');
 
     navMenu.classList.remove('page-header__navigation--closed');
-    navLogo[0].setAttribute('hidden', 'hidden');
-    navLogo[1].removeAttribute('hidden', 'hidden');
+
+    if (navLogo[0].getAttribute('hidden') === null && !navMenu.classList.contains('page-header__navigation--scrolled')) {
+      console.log(navLogo[0].getAttribute('hidden'));
+      console.log(navLogo[1].getAttribute('hidden'));
+
+      navLogo[0].setAttribute('hidden', 'hidden');
+      navLogo[1].removeAttribute('hidden', 'hidden');
+    }
 
     navButton.classList.toggle('page-header__menu-button--opened');
     navButtonText[0].setAttribute('hidden', 'hidden');
     navButtonText[1].removeAttribute('hidden', 'hidden');
 
     navMenuElement.forEach(element => {
-      element.classList.remove('page-header__menu-element--closed');
+      // element.classList.remove('page-header__menu-element--closed');
+      if (element.classList.contains('page-header__nav-list')) {
+        element.classList.remove('page-header__nav-list--closed')
+      }
+      if (element.classList.contains('page-header__auth-item')) {
+        element.classList.remove('page-header__auth-item--closed')
+      }
+      if (element.classList.contains('page-header__socials-list')) {
+        element.classList.remove('page-header__socials-list--closed')
+      }
     });
-
-
   } else {
     console.log('меню закрывается');
 
     navMenu.classList.toggle('page-header__navigation--closed');
-    navLogo[1].setAttribute('hidden', 'hidden');
-    navLogo[0].removeAttribute('hidden', 'hidden');
+
+    if (navLogo[1].getAttribute('hidden') === null && !navMenu.classList.contains('page-header__navigation--scrolled')) {
+      console.log(navLogo[0].getAttribute('hidden'));
+      console.log(navLogo[1].getAttribute('hidden'));
+
+      navLogo[1].setAttribute('hidden', 'hidden');
+      navLogo[0].removeAttribute('hidden', 'hidden');
+    }
 
     navButton.classList.remove('page-header__menu-button--opened');
     navButtonText[1].setAttribute('hidden', 'hidden');
     navButtonText[0].removeAttribute('hidden', 'hidden');
 
     navMenuElement.forEach(element => {
-      element.classList.toggle('page-header__menu-element--closed');
+      // element.classList.toggle('page-header__menu-element--closed');
+      if (element.classList.contains('page-header__nav-list')) {
+        element.classList.toggle('page-header__nav-list--closed')
+      }
+      if (element.classList.contains('page-header__auth-item')) {
+        element.classList.toggle('page-header__auth-item--closed')
+      }
+      if (element.classList.contains('page-header__socials-list')) {
+        element.classList.toggle('page-header__socials-list--closed')
+      }
     });
-
-
   }
 })
