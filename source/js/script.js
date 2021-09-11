b96bc398cf57f28dcde79807ffd0f3778f681064
Electr0Bear <dmitry.kn@protonmail.com>
2021-09-11 16:48:08 +0300

    добавляет событие по клику на выбор страны + стили

--- source/js/script.js
+++ source/js/script.js
@@ -83,32 +83,64 @@ navButton.addEventListener('click', evt => {
   }
 });
 
-// Тарифы для бизнеса
+if (window.location.href.indexOf('/index.html') !== -1) {
+  console.log('Главная');
+  // Тарифы для бизнеса
 
-const businessRatesShowBtn = document.querySelector('.profile-rates__business-rates-link');
-const businessRatesCloseBtn = document.querySelector('.profile-rates__business-close-button');
-const businessRates = document.querySelector('.profile-rates__business-popup');
+  const businessRatesShowBtn = document.querySelector('.profile-rates__business-rates-link');
+  const businessRatesCloseBtn = document.querySelector('.profile-rates__business-close-button');
+  const businessRates = document.querySelector('.profile-rates__business-popup');
 
 
-businessRatesShowBtn.addEventListener('click', evt => {
-  evt.preventDefault();
-  if (!businessRates.classList.contains('profile-rates__business-popup--show')) {
-    businessRates.classList.toggle('profile-rates__business-popup--show');
-  }
-});
+  businessRatesShowBtn.addEventListener('click', evt => {
+    evt.preventDefault();
+    if (!businessRates.classList.contains('profile-rates__business-popup--show')) {
+      businessRates.classList.toggle('profile-rates__business-popup--show');
+    }
+  });
 
-businessRatesCloseBtn.addEventListener('click', evt => {
-  evt.preventDefault();
-  if (businessRates.classList.contains('profile-rates__business-popup--show')) {
-    businessRates.classList.remove('profile-rates__business-popup--show');
-  }
-});
-
-document.addEventListener('keyup', evt => {
-  if (evt.code === 'Escape') {
+  businessRatesCloseBtn.addEventListener('click', evt => {
     evt.preventDefault();
     if (businessRates.classList.contains('profile-rates__business-popup--show')) {
       businessRates.classList.remove('profile-rates__business-popup--show');
     }
-  }
-});
+  });
+
+  document.addEventListener('keyup', evt => {
+    if (evt.code === 'Escape') {
+      evt.preventDefault();
+      if (businessRates.classList.contains('profile-rates__business-popup--show')) {
+        businessRates.classList.remove('profile-rates__business-popup--show');
+      }
+    }
+  });
+}
+
+if (window.location.href.indexOf('/form.html') !== -1) {
+  console.log('Направления');
+  // Выпадающий список стран на странице формы
+
+  const countryCurrentInput = document.querySelector('.step-two__countries-select-current');
+  const countryInputDroplist = document.querySelector('.step-two__countries-droplist');
+
+  countryCurrentInput.addEventListener('click', evt => {
+    evt.preventDefault();
+    if (!countryCurrentInput.classList.contains('step-two__countries-select-current--active')) {
+      countryCurrentInput.classList.toggle('step-two__countries-select-current--active');
+      countryInputDroplist.classList.toggle('step-two__countries-droplist--show');
+    } else if (countryCurrentInput.classList.contains('step-two__countries-select-current--active')) {
+      countryCurrentInput.classList.remove('step-two__countries-select-current--active');
+      countryInputDroplist.classList.remove('step-two__countries-droplist--show');
+    }
+  });
+
+  document.addEventListener('keyup', evt => {
+    if (evt.code === 'Escape') {
+      evt.preventDefault();
+      if (countryCurrentInput.classList.contains('step-two__countries-select-current--active')) {
+        countryCurrentInput.classList.remove('step-two__countries-select-current--active');
+        countryInputDroplist.classList.remove('step-two__countries-droplist--show');
+      }
+    }
+  });
+}
