377f160bd513b8ac9665f3e9a1126215c6de323b
Electr0Bear <dmitry.kn@protonmail.com>
2021-09-15 14:09:41 +0300

    добавляет автоматическую сборку билда

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,9 @@ const htmlmin = require("gulp-htmlmin");
 const csso = require("postcss-csso");
 const rename = require("gulp-rename");
 const terser = require("gulp-terser");
+const squoosh = require("gulp-libsquoosh");
+const webp = require("gulp-webp");
+const del = require("del");
 
 // Styles
 
@@ -18,10 +21,12 @@ const styles = () => {
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
+    .pipe(rename("/style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
@@ -38,19 +43,8 @@ const html = () => {
 };
 exports.html = html;
 
-// CSS
-
-const cssmin = () => {
-  return gulp.src('source/css/style.css')
-    .pipe(postcss([
-      csso()
-    ]))
-    .pipe(rename("/style.min.css"))
-    .pipe(gulp.dest("build/css"));
-}
-exports.cssmin = cssmin;
-
 // JS
+
 const scriptmin = () => {
   return gulp.src('source/js/script.js')
     .pipe(terser())
@@ -59,15 +53,61 @@ const scriptmin = () => {
 }
 exports.scriptmin = scriptmin;
 
+// Images
 
+const imagesOptimise = () => {
+  return gulp.src('source/img/**/*.{jpg,png,svg}')
+    .pipe(squoosh())
+    .pipe(gulp.dest("build/img"));
+}
+exports.imagesOptimise = imagesOptimise;
 
+// Images-copy
+
+const imagesCopy = done => {
+  gulp.src("source/img/**/*.{jpg,png,svg}", {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+  done();
+}
+exports.imagesCopy = imagesCopy;
+
+// Webp
+
+const imagesWebp = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(webp({
+      quality: 90
+    }))
+    .pipe(gulp.dest("build/img"));
+}
+exports.imagesWebp = imagesWebp;
+
+//Copy
+
+const copy = done => {
+  gulp.src("source/fonts/*.{woff,woff2}", {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+  done();
+}
+exports.copy = copy;
+
+//Clean
+
+const clean = () => {
+  return del("build");
+}
+exports.clean = clean;
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -78,13 +118,48 @@ const server = (done) => {
 
 exports.server = server;
 
+// Reload
+
+const reload = done => {
+  sync.reload();
+  done();
+}
+
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
+  gulp.watch("source/js/script.js", gulp.series(scriptmin));
+  gulp.watch("source/*.html", gulp.series(html, reload));
 }
 
+// Build
+
+const build = gulp.series(
+  clean,
+  copy,
+  imagesOptimise,
+  gulp.parallel(
+    styles,
+    html,
+    scriptmin,
+    imagesWebp
+  )
+);
+exports.build = build;
+
 exports.default = gulp.series(
-  styles, server, watcher
+  clean,
+  copy,
+  imagesCopy,
+  gulp.parallel(
+    styles,
+    html,
+    scriptmin,
+    imagesWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  )
 );
