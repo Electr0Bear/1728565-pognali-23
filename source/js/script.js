d133cb45f48f63106cb1663db103a490bade23ab
Electr0Bear <dmitry.kn@protonmail.com>
2021-09-16 17:02:03 +0300

    добавляет опцию постоянно открытой шапки при незагруженном JS (Б30)

--- source/js/script.js
+++ source/js/script.js
@@ -6,6 +6,29 @@ const navMenu = document.querySelector('.page-header__navigation');
 const navMenuElement = document.querySelectorAll('.page-header__nav-list, .page-header__auth-item, .page-header__socials-list');
 const navLogo = document.querySelectorAll('.page-header__logo-regular, .page-header__logo-blue');
 
+if (navMenu.classList.contains('page-header__navigation--preopened')) {
+  navButton.classList.remove('page-header__menu-button--opened');
+  navButtonText[1].setAttribute('hidden', 'hidden');
+  navButtonText[0].removeAttribute('hidden', 'hidden');
+
+  navMenuElement.forEach(element => {
+    if (element.classList.contains('page-header__nav-list')) {
+      element.classList.toggle('page-header__nav-list--closed')
+    }
+    if (element.classList.contains('page-header__auth-item')) {
+      element.classList.toggle('page-header__auth-item--closed')
+    }
+    if (element.classList.contains('page-header__socials-list')) {
+      element.classList.toggle('page-header__socials-list--closed')
+    }
+  });
+
+  navLogo[1].setAttribute('hidden', 'hidden');
+  navLogo[0].removeAttribute('hidden', 'hidden');
+
+  navMenu.classList.remove('page-header__navigation--preopened');
+}
+
 document.addEventListener('scroll', () => {
   if (window.pageYOffset > 60 && !navMenu.classList.contains('page-header__navigation--opened') && !navMenu.classList.contains('page-header__navigation--scrolled')) {
     navMenu.classList.toggle('page-header__navigation--scrolled');
@@ -40,7 +63,6 @@ navButton.addEventListener('click', evt => {
     navButtonText[1].removeAttribute('hidden', 'hidden');
 
     navMenuElement.forEach(element => {
-      // element.classList.remove('page-header__menu-element--closed');
       if (element.classList.contains('page-header__nav-list')) {
         element.classList.remove('page-header__nav-list--closed')
       }
@@ -69,7 +91,6 @@ navButton.addEventListener('click', evt => {
     navButtonText[0].removeAttribute('hidden', 'hidden');
 
     navMenuElement.forEach(element => {
-      // element.classList.toggle('page-header__menu-element--closed');
       if (element.classList.contains('page-header__nav-list')) {
         element.classList.toggle('page-header__nav-list--closed')
       }
